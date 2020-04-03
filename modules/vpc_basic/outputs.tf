47caca0eff202ca7d5bd8f8e64547e38bad3fa9e
lbgc <lbrealdeveloper@gmail.com>
2020-04-03 02:41:54 +0200

    add: outputs vpc_basic

--- modules/vpc_basic/outputs.tf
+++ modules/vpc_basic/outputs.tf
@@ -1,3 +1,20 @@
+output "this_vpc_id" {
+  value = concat(aws_vpc.main.*.id, [""])[0]
+}
+
+output "this_vpc_cidr" {
+  value = concat(aws_vpc.main.*.cidr_block, [""])[0]
+}
+
+output "this_subnet_private_cidrs" {
+  value = concat(aws_subnet.private.*.cidr_block, [""])[0]
+}
+
+output "this_subnet_public_cidrs" {
+  value = concat(aws_subnet.public.*.cidr_block, [""])[0]
+}
+
+
 /*
 provider "aws" {
   region  = "eu-central-1"
