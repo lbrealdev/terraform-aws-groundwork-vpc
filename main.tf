d40433968c6b47606d467f7f77d83b8af73a4ae1
lbgc <lbrealdeveloper@gmail.com>
2020-04-03 02:37:09 +0200

    update: .gitignore/plan files

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@
 # THESE TEMPLATES REQUIRE TERRAFORM VERSION 0.12 AND ABOVE
 # ----------------------------------------------------------------------------------------------
 
+provider "aws" {
+  region = "eu-central-1"
+}
+
 module "vpc" {
   source = "./modules/vpc_basic"
 
