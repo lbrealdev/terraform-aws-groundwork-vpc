0176888982184b0bee932ce33c05a65f76df07c5
lbgc <lbrealdeveloper@gmail.com>
2020-04-04 19:10:13 +0200

    update: resources vpc

--- modules/vpc_basic/main.tf
+++ modules/vpc_basic/main.tf
@@ -7,11 +7,12 @@ terraform {
 }
 
 locals {
+  workspace = terraform.workspace
+
   tags = {
     Description = "Managed by Terraform"
+    Environment = local.workspace
   }
-
-  workspace = terraform.workspace
 }
 
 resource "aws_vpc" "main" {
@@ -22,12 +23,30 @@ resource "aws_vpc" "main" {
   enable_dns_support   = var.enable_dns_support
 
   tags = merge(
+    local.tags,
     {
-      Name        = "vpc-${local.workspace}-basic"
-      Description = local.workspace
+      Name = "vpc-${local.workspace}-basic"
     }
   )
+}
 
+resource "aws_vpc_dhcp_options" "main" {
+  count = var.create_vpc && var.domain_name == var.domain_name_servers != "" ? 1 : 0
+
+  domain_name         = var.domain_name
+  domain_name_servers = var.domain_name_servers
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "dhcp-${local.workspace}"
+    }
+  )
+}
+
+resource "aws_vpc_dhcp_options_association" "main" {
+  dhcp_options_id = aws_vpc_dhcp_options.main[0].id
+  vpc_id          = aws_vpc.main[0].id
 }
 
 data "aws_availability_zones" "main" {
@@ -42,12 +61,11 @@ resource "aws_subnet" "private" {
   availability_zone = concat(data.aws_availability_zones.main.names, [""])[count.index]
 
   tags = merge(
+    local.tags,
     {
-      Name        = "vpc-${local.workspace}-private"
-      Description = local.workspace
+      Name ="subnet-${local.workspace}-private"
     }
   )
-
 }
 
 resource "aws_subnet" "public" {
@@ -59,9 +77,71 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 
   tags = merge(
+    local.tags,
     {
-      Name        = "subnet-${local.workspace}-public"
-      Description = local.workspace
+      Name = "subnet-${local.workspace}-public"
     }
   )
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main[0].id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "igw-${local.workspace}"
+    }
+  )
+}
+
+resource "aws_eip" "main" {
+  count = var.create_vpc && length(aws_internet_gateway.main) > 0 ? length(aws_subnet.public) : 0
+
+  vpc = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "eip-${local.workspace}"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "main" {
+  count = var.create_vpc ? 3 : 0
+
+  subnet_id     = aws_subnet.public[count.index].id
+  allocation_id = element(aws_eip.main.*.id, count.index)
+}
+
+resource "aws_route_table" "main" {
+  count = var.create_vpc && length(aws_internet_gateway.main) > 0 ? length(aws_nat_gateway.main) : 0
+
+  vpc_id = aws_vpc.main[0].id
+
+  route {
+    cidr_block     = var.rtb_cidr_block
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "rtb-${local.workspace}"
+    }
+  )
+}
+
+resource "aws_route_table_association" "main" {
+  count = length(aws_route_table.main)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.main[count.index].id
+}
+
+resource "aws_route" "main" {
+  route_table_id         = aws_vpc.main[0].main_route_table_id
+  gateway_id             = aws_internet_gateway.main.id
+  destination_cidr_block = var.rtb_cidr_block
 }
--- modules/vpc_basic/variables.tf
+++ modules/vpc_basic/variables.tf
@@ -33,3 +33,20 @@ variable "enable_dns_support" {
   type        = bool
   default     = true
 }
+
+variable "domain_name" {
+  description = " the suffix domain name to use by default when resolving non Fully Qualified Domain Names. In other words, this is what ends up being the search value in the /etc/resolv.conf file."
+  type        = string
+  default     = "eu-central-1.compute.internal"
+}
+
+variable "domain_name_servers" {
+  description = ""
+  type        = list(string)
+  default     = ["AmazonProvidedDNS"]
+}
+
+variable "rtb_cidr_block" {
+  type    = string
+  default = "0.0.0.0/0"
+}
